0a94f687c9e5d222137454efec0d0b0d764403fa
IlyaOsotov <soten58@yandex.ru>
2021-08-20 17:03:55 +0700

    Light app

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    @IBOutlet var lightButton: UIButton!
+    
+    fileprivate func toggleBackground() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    fileprivate func changeButtonName() {
+        let newTitleName = lightOn ? "on" : "off"
+        lightButton.setTitle(newTitleName, for: .normal)
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        toggleBackground()
+        changeButtonName()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
